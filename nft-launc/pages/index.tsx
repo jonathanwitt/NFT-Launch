76f9e013942cdf47355db7225cb6f3941498a7da
jonathanwitt <joniwitt79@gmail.com>
2022-05-15 18:13:21 +0300

    looks like im done with front end and metamask wallet connected with thirdweb

---
+++ nft-launc/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>NFT Drop</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1 className="font-bold text-red-500">Welcome to the NFT DROP</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ nft-launc/pages/search.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function search() {
+  return <div>Search Page</div>
+}
+
+export default search
